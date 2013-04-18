b3ab9e30fbec0d15e9a1e3ad04fec8b288f04e45
naveedmurtuza <naveedmurtuza@f74c8d93-bca8-261f-860f-a889c0cca687>
2013-04-18 18:30:33 +0000

    Refactor [out dir removed]

--- trunk/src/com/xdxf/dictionary/BookListAdapter.java
+++ trunk/src/com/xdxf/dictionary/BookListAdapter.java
@@ -1,7 +1,6 @@
 package com.xdxf.dictionary;
 
 import android.app.Activity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
